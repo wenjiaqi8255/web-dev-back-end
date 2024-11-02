4e382bf35c1492974f4b8b52c63b1b8e769e79e3
Jiaqi Wen <127540860+wenjiaqi8255@users.noreply.github.com>
2024-11-02 19:54:22 +0100

    Remove nested git repositories and reorganize project structure

--- 2.2 Native Modules/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("message.txt","utf-8",(err,data) => {
-    if (err) throw err;
-    console.log(data);
-  });
--- 2.3 NPM/index.js
+++
@@ -1,5 +0,0 @@
-import superheroes from "superheroes";
-
-var randomName=superheroes.random();
-
-console.log(`Your name is ${randomName}.`);
